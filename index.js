aebf569bca38404db48069b329aea9cae78067ee
Keegan Nair <Keegan.nair@hotmail.com>
2021-10-12 18:08:13 +1100

    Adding starter code for early blackjack logic

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 console.log("Javascript connected");
 
 
-let firstCard = 6;
-let secondCard = 9;
+let firstCard = 20;
+let secondCard = 1;
 
 let sum = firstCard + secondCard;
 
-console.log(sum);
+//console.log(sum);
+
+
+if (sum < 21){
+  console.log("Do you want to draw a new card?");
+} else if(sum === 21){
+  console.log("You got BlackJack!");
+} else {
+  console.log("You are out of the game");
+}
