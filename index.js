1d90e85a576301b6dbf91f6ff416a3f05512889d
Keegan Nair <Keegan.nair@hotmail.com>
2021-10-13 14:36:30 +1100

    linking html to message in JS file

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ let firstCard = 20;
 let secondCard = 1;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
+
+let message = "";
+let messageEl = document.getElementById("message-el");
 //console.log(sum);
 
 let isAlive = true;
-let message = "";
-
-if (sum < 21){
-  message = "Draw a new card?";
-  
-} else if(sum === 21){
-  message = "You got BlackJack!"
-  hasBlackJack = true;
-  
-} else {
-  message = "you went over 21";
-  isAlive = false;
-}
-
-console.log(message);
 
 function startGame(){
   console.log("btn clicked");
+  if (sum < 21){
+    message = "Draw a new card?";
+    
+  } else if(sum === 21){
+    message = "You got BlackJack!"
+    hasBlackJack = true;
+    
+  } else {
+    message = "you went over 21";
+    isAlive = false;
+  }
+  console.log(message);
+  messageEl.textContent = message;
 } 
 
